dbf13ac16dc09fc47b331da18ac7020995b6d8ab
“hushao” <991426834@qq.com>
2024-08-26 11:00:14 +0800

    少了几段代码，更新上传

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,57 @@
+<template>
+    <el-menu
+      class="el-menu-vertical-demo"
+      router
+      background-color="#f5f5f5"
+      text-color="#333"
+      active-text-color="#409EFF"
+      :default-active="activeMenu">
+      <el-menu-item index="/user-management">
+        <i class="el-icon-user"></i>
+        <span slot="title">用户管理</span>
+      </el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-setting"></i>
+          <span>权限管理</span>
+        </template>
+        <el-menu-item index="/permission-management/overview">
+          <i class="el-icon-lock"></i>
+          <span>权限概览</span>
+        </el-menu-item>
+        <el-menu-item index="/permission-management/own">
+          <i class="el-icon-s-unfold"></i>
+          <span>我的权限</span>
+        </el-menu-item>
+      </el-submenu>
+      <el-menu-item index="/statistics">
+        <i class="el-icon-data-analysis"></i>
+        <span slot="title">数据统计</span>
+      </el-menu-item>
+    </el-menu>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SidebarMenu',
+    data() {
+      return {
+        activeMenu: this.$route.path
+      };
+    },
+    watch: {
+      $route(to) {
+        this.activeMenu = to.path;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .el-menu-vertical-demo {
+    width: 200px;
+    min-height: 100%;
+    border-right: 1px solid #e6e6e6;
+  }
+  </style>
+  
---
+++ src/views/Statistics.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <el-card>
+        <div slot="header" class="clearfix">
+          <span>数据统计</span>
+        </div>
+        <div>
+          <el-row>
+            <el-col :span="12">
+              <div id="user-stats-chart" style="height: 400px;"></div>
+            </el-col>
+          </el-row>
+        </div>
+      </el-card>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'UserStatistics',
+    mounted() {
+      this.initChart();
+    },
+    methods: {
+      initChart() {
+        // 使用图表库绘制统计图表
+      }
+    }
+  };
+  </script>
+  
